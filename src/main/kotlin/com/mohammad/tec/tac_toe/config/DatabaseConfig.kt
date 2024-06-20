f5df8e7921146c23fad8bdc2e48ef5ab9fe4cdac
Mohammad-Al-Refai <m.shebli.refaai@gmail.com>
2024-06-20 22:58:02 +0300

    add schema

---
+++ src/main/kotlin/com/mohammad/tec/tac_toe/config/DatabaseConfig.kt
@@ -0,0 +1,27 @@
+package com.mohammad.tec.tac_toe.config
+
+import io.r2dbc.spi.ConnectionFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
+
+@Configuration
+@EnableR2dbcRepositories
+class DatabaseConfig {
+    @Bean
+    fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
+        val initializer = ConnectionFactoryInitializer()
+        initializer.setConnectionFactory(connectionFactory)
+        initializer.setDatabasePopulator(
+            ResourceDatabasePopulator(
+                ClassPathResource("schema.sql")
+            )
+        )
+        return initializer
+    }
+
+
+}
